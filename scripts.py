dc9bd448232e3981eddd74de0a4860c2dfe6342d
VASILIYKAS <yoyoynv@gmail.com>
2025-01-16 16:02:02 +0500

    Подправил имя переменной в функции `find_student`. Добавил проверку наличия предметов в функции `create_commendation`, вынес похвалу в константу.

--- scripts.py
+++ scripts.py
@@ -1,16 +1,25 @@
+COMMENDATION = [
+    'Молодец!', 
+    'Отлично!', 
+    'Хорошо!',
+    'Великолепно!', 
+    'Прекрасно!', 
+    'Потрясающе!',
+]
+
+
 def find_student(student_name):
     """Поиск ученика"""
     student = Schoolkid.objects.filter(full_name__contains=student_name)
     if student.count() == 0:
         print(f'Ученик с именем "{student_name}" не найден.')
     elif student.count() == 1:
-        full_name = student.get()
-        print(f'Найден ученик с именем {student_name.full_name}')
-        return full_name
+        name = student.get()
+        print(f'Найден ученик с именем {name.full_name}')
+        return name
     else:
         print(f'''Найдено несколько учеников с именем "{student_name}". 
             Пожалуйста, уточните запрос.''')
-
 
 
 def fix_marks(student_name):
@@ -37,15 +46,16 @@ def create_commendation(schoolkid, date, year_of_study, group_letter):
     """Добавление похвалы от учителей"""
     lessons = Lesson.objects.filter(
         year_of_study=year_of_study, group_letter=group_letter, date=date)
+    if not lessons:
+        return f'Похоже {date} уроков нет. Попробуйте другую дату'
+    
     random_lesson = lessons.order_by('?').first()
     lesson_title = random_lesson.subject.title
     teachers = Lesson.objects.filter(
         subject__title=lesson_title, year_of_study=year_of_study)
     teacher = teachers.first().teacher
     subject = Subject.objects.filter(title=lesson_title).first()
-    commendation = ['Молодец!', 'Отлично!', 'Хорошо!',
-                    'Великолепно!', 'Прекрасно!', 'Потрясающе!']
-    random_commendation = random.choice(commendation)
+    random_commendation = random.choice(COMMENDATION)
     Commendation.objects.create(
         text=random_commendation,
         created=date,
